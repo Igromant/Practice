356b91c87454eeb4dafe62cf6a33c1736e8512f6
Igor Golubev <igruliman@gmail.com>
2021-12-30 14:04:31 +0300

    Задание 02.1 выполнено

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-const tititle = 0;
-const screens = 1;
-const screenPrice  = 2;
-const rollback = 3;
-const fullPrice = 4;
-const adaptive = 5;
+const title = "Over Working";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice  = 450;
+const rollback = 65;
+const fullPrice = 35000;
+const adaptive = true;
 
-alert("Привет геймеры мира!");
+console.log(title);
 
-console.log('Передаю слово Эврику!');
+console.log(fullPrice);
+
+console.log(adaptive);
+
+console.log(title, fullPrice, adaptive);
+
+console.log(screens.length);
+
+console.log('“Стоимость верстки экранов '+ screenPrice +' рублей/ долларов/гривен/юани”' );
+
+console.log('“Стоимость разработки сайта '+ fullPrice +' рублей/ долларов/гривен/юани”' );
+
+console.log(screens.toLowerCase());
+
+console.log(screens.split());
+
+console.log(screens.toLowerCase().split());
+
+console.log(fullPrice * (rollback/100));
