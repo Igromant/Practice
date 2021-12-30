b6e9f2f8b8328d94e0311b00609ad6ac8bb679bc
Igor Golubev <igruliman@gmail.com>
2021-12-30 14:01:07 +0300

    Задание 01 выполнено

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const tititle = 0;
+const screens = 1;
+const screenPrice  = 2;
+const rollback = 3;
+const fullPrice = 4;
+const adaptive = 5;
+
+alert("Привет геймеры мира!");
+
+console.log('Передаю слово Эврику!');
