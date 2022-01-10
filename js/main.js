32030b39e57cfb1373d9f0ceb4d19f12dd7a5b68
Igor Golubev <igruliman@gmail.com>
2022-01-10 16:31:12 +0300

    Задание 05.ex2 выполнено

--- js/main.js
+++ js/main.js
@@ -1,110 +1,36 @@
 'use strict';
 
-//Объявление переменных
+// Доп задание 2
 
-let title
-let adaptive
-let screens
-let screenPrice
-const rollback = 65;
-let service1
-let servicePrice1
-let service2
-let servicePrice2
-let fullPrice
-let servicePrcentPrice
-let allServicePrices
+const arr = ['678208', '234765', '325634', '238928', '7893463', '4798367', '2348423'];
 
-
-
-//Функции
-const isNumber = function(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num)
-}
-
-const asking = function() {
-    title = prompt(' “Как называется ваш проект?” ', 'Halograme');
-    screens = prompt(' “Какие типы экранов нужно разработать?” ', 'Простые, Сложные, Интерактивные');
-
-    do {
-        screenPrice  = prompt(' “Сколько будет стоить данная работа?” ');
-    } while (!isNumber(screenPrice))
-
-    adaptive = confirm(' “Нужен ли адаптив на сайте? ” ');
-}
-
-const getAllServicePrices = function() {
-    let sum = 0
-
-    for (let i = 0; i < 2; i++) {
-    let price = 0
-
-        if( i === 0 ){
-            service1 = prompt(' “Какой дополнительный тип услуги нужен?” ');
-        } else if ( i === 1 ){
-            service2 = prompt(' “Какой дополнительный тип услуги нужен?” ');
-        }
-
-        do {
-            price = prompt('Сколько это будет стоить?');
-          } while (!isNumber(price))
-      
-          sum += +price
-      
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
+        console.log(arr[i]);
     }
-    return sum
 }
 
-const showTypeOf = function(variable) {
-    console.log(variable, typeof variable);
+const divi = el => {
+    let arr = [];
+    if (el !== 1) {
+        arr.push(1);
+        for (let j = 2; j * j <= el; j++) {
+            if (el % j === 0) {
+                arr.push(j);
+            }
+        }
+    }
+    arr.push(el);
+    return arr;
+};
+
+for (let i = 1; i <= 100; i++) {
+    const n = divi(i);
+    if (n.length <= 2) {
+        console.log(`${i}: Простое число. Делители этого числа: ${n.join(', ')}`);
+    }
 }
 
-const getRollbackMessage = function(price) {
-    if (price >= 30000) {
-        return 'Даем скидку в 10%'
-  } else if (price >= 15000 && price < 30000) {
-    return 'Даем скидку в 5%'
-  } else if (price >= 0 && price < 15000) {
-    return 'Скидка не предусмотрена'
-  } else {
-    return 'Что-то пошло не так'
-  }
+for (let i = 1; i <= 100; i++) {
+    console.log(`${i}: Делители этого числа: ${divi(i).join(', ')}`);
 }
-
-function getFullPrice(a,b) {
-    return Number(a)+Number(b)
-} 
-
-const getServicePercentPrices = function(a,b) {
-    return Math.ceil(a-a*(b/100))
-}
-
-const getTitle = function() {
-    return title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase()
-}
-
-
-
-asking();
-title = getTitle();
-allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice(screenPrice,allServicePrices);
-servicePrcentPrice = getServicePercentPrices(fullPrice,rollback);
-
-
-
-
-//Вывод в консоль
-
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
-
-console.log(screens.length);
-
-console.log(servicePrcentPrice);
-console.log(allServicePrices);
-console.log(getRollbackMessage(fullPrice));
-
-console.log('“Стоимость верстки экранов '+ screenPrice +' рублей/ долларов/гривен/юани”' );
-console.log('“Стоимость разработки сайта '+ fullPrice +' рублей/ долларов/гривен/юани”' );
