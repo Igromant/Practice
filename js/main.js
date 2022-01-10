67d2bf5ca5f5807a0b0d297b7180c8080d1eb21b
Igor Golubev <igruliman@gmail.com>
2022-01-10 15:27:33 +0300

    Задание 04.ex выполнено

--- js/main.js
+++ js/main.js
@@ -1,99 +1,25 @@
 'use strict';
 
-//Объявление переменных
+//Доп Задание
 
-let title
-let adaptive
-let screens
-let screenPrice
-const rollback = 65;
-let service1
-let servicePrice1
-let service2
-let servicePrice2
-let fullPrice
-let servicePrcentPrice
-let allServicePrices
+let offer = prompt('Введите какое либо предложение.');
 
-
-
-//Функции
-
-const ask = function() {
-    title = prompt(' “Как называется ваш проект?” ');
-    screens = prompt(' “Какие типы экранов нужно разработать?” ', ' "Простые, Сложные, Интерактивные" ');
-    screenPrice  = +prompt(' “Сколько будет стоить данная работа?” ', '12000');
-
-    adaptive = confirm(' “Нужен ли адаптив на сайте? ”');
-}
-
-const getAllServicePrices = function() {
-    let allPrices = 0;
-
-    for (let i = 0; i < 2; i++) {
-
-        if( i == 0 ){
-            service1  = prompt(' “Какой дополнительный тип услуги нужен?” ');
-        } else if ( i == 1 ){
-            service2  = prompt(' “Какой дополнительный тип услуги нужен?” ');
+const fun = (arg) => {
+    let res = 'Ожидается появление строки...';
+    if(typeof arg == "string"){
+        const maxLength = 30;
+        const str = arg.trim();
+        if(str.length){
+            res = str.substr(0,maxLength)+'...';
+        } else {
+            res = str;
         }
-
-        allPrices += +prompt(' “Сколько это будет стоить?” ');
-        
     }
-    return allPrices
+
+    return res;
 }
 
-const showTypeOf = function(variable) {
-    console.log(variable, typeof variable);
-}
-
-const getRollbackMessage = function(price) {
-    if (price >= 30000) {
-        return 'Даем скидку в 10%'
-  } else if (price >= 15000 && price < 30000) {
-    return 'Даем скидку в 5%'
-  } else if (price >= 0 && price < 15000) {
-    return 'Скидка не предусмотрена'
-  } else {
-    return 'Что-то пошло не так'
-  }
-}
-
-function getFullPrice(a,b) {
-    return Number(a)+Number(b)
-} 
-
-const getServicePercentPrices = function(a,b) {
-    return Math.ceil(a-a*(b/100))
-}
-
-const getTitle = function() {
-    return title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase()
-}
-
-
-
-ask();
-title = getTitle();
-allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice(screenPrice,allServicePrices);
-servicePrcentPrice = getServicePercentPrices(fullPrice,rollback);
-
-
-
-
-//Вывод в консоль
-
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
-
-console.log(screens.length);
-
-console.log(servicePrcentPrice);
-console.log(allServicePrices);
-console.log(getRollbackMessage(fullPrice));
-
-console.log('“Стоимость верстки экранов '+ screenPrice +' рублей/ долларов/гривен/юани”' );
-console.log('“Стоимость разработки сайта '+ fullPrice +' рублей/ долларов/гривен/юани”' );
+console.log('fun(): ', fun(5));
+console.log('fun(): ', fun('    Строка менее 30 символов     '));
+console.log('fun(): ', fun('    Строка более 30 символов - ну очень при очень длинная строчка получилась!'));
+console.log('fun(): ', fun(offer));
