d2a08273b086345ec0174ca57770f91c08e19b02
Igor Golubev <igruliman@gmail.com>
2022-01-11 10:52:53 +0300

    Задание 03 пере выполнено

--- js/main.js
+++ js/main.js
@@ -1,25 +1,20 @@
 'use strict';
 
+// Объявление переменных
+
 let title = prompt(' “Как называется ваш проект?” ');
 let adaptive = prompt(' “Нужен ли адаптив на сайте? ”', 'true/false');
 let screens = prompt(' “Какие типы экранов нужно разработать?” ', ' "Простые, Сложные, Интерактивные" ');
 let screenPrice  = +prompt(' “Сколько будет стоить данная работа?” ', '12000');
 const rollback = 65;
-
-// Задание 7
-
 let service1  = prompt(' “Какой дополнительный тип услуги нужен?” ');
 let servicePrice1  = +prompt(' “Сколько это будет стоить?” ');
 let service2  = prompt(' “Какой дополнительный тип услуги нужен?” ');
 let servicePrice2  = +prompt(' “Сколько это будет стоить?” ');
-
 let fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
-
-// fullPrice amount
-
 let servicePrcentPrice = Math.ceil(fullPrice-fullPrice * (rollback/100))
 
-// Data
+// Условие
 
 if (fullPrice >= 30000) {
     console.log('Даем скидку в 10%');
@@ -31,36 +26,21 @@ if (fullPrice >= 30000) {
     console.log('Что-то пошло не так');
   }
 
-console.log(title, fullPrice, adaptive);
 
-// title, fullPrice and adaptive
+// Вывод в консоль
 
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
 console.log('“Стоимость верстки экранов '+ screenPrice +' рублей/ долларов/гривен/юани”' );
-
-// PriceScreens
-
 console.log('“Стоимость разработки сайта '+ fullPrice +' рублей/ долларов/гривен/юани”' );
-
-// FullPrice
-
 console.log(screens.length);
-
 console.log(screens.toLowerCase());
-
 console.log(screens.split());
-
 console.log(screens.toLowerCase().split());
-
-// Screens
-
 console.log(fullPrice * (rollback/100));
-
-// Rollback
-
 console.log(service1, servicePrice1);
-
 console.log(service2, servicePrice2);
-
 console.log(servicePrcentPrice);
 
 
