28dbf2048fd7180b709941eb604d264d97a4e516
Igor Golubev <igruliman@gmail.com>
2022-01-11 10:45:59 +0300

    Задание 02 пере выполнено

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Объявление переменных
 const title = "Over Working";
 const screens = "Простые, Сложные, Интерактивные";
 const screenPrice  = 450;
@@ -5,24 +6,16 @@ const rollback = 65;
 const fullPrice = 35000;
 const adaptive = true;
 
+
+//Вывод в консоль
 console.log(title);
-
 console.log(fullPrice);
-
 console.log(adaptive);
-
 console.log(title, fullPrice, adaptive);
-
 console.log(screens.length);
-
 console.log('“Стоимость верстки экранов '+ screenPrice +' рублей/ долларов/гривен/юани”' );
-
 console.log('“Стоимость разработки сайта '+ fullPrice +' рублей/ долларов/гривен/юани”' );
-
 console.log(screens.toLowerCase());
-
 console.log(screens.split());
-
 console.log(screens.toLowerCase().split());
-
 console.log(fullPrice * (rollback/100));
