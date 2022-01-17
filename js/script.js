3f7c676d787e6b1f7c5bdf06e89d0fa5d53bdc9a
Igor Golubev <igruliman@gmail.com>
2022-01-17 11:39:41 +0300

    Задание 13 todo

--- js/script.js
+++ js/script.js
@@ -2,160 +2,82 @@
 
 // Объявление переменных
 
-const title = document.getElementsByTagName('h1')[0]
-const buttonPlus = document.querySelector('.screen-btn')
-const otherItemsPercent = document.querySelectorAll('.other-items.percent')
-const otherItemsNumber = document.querySelectorAll('.other-items.number')
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const toDoList = document.querySelector('.todo-list');
+const toDoCompleted = document.querySelector('.todo-completed');
 
-const inputRange = document.querySelector('.rollback input')
-const inputRangeValue = document.querySelector('.rollback .range-value')
+let toDoData = [];
 
-const startBtn = document.getElementsByClassName('handler_btn')[0]
-const resetBtn = document.getElementsByClassName('handler_btn')[1]
+const render = function () {
+  toDoList.innerHTML = ''
+  toDoCompleted.innerHTML = ''
 
-const total = document.getElementsByClassName('total-input')[0]
-const totalCount = document.getElementsByClassName('total-input')[1]
-const totalCountOther = document.getElementsByClassName('total-input')[2]
-const fullTotalCount = document.getElementsByClassName('total-input')[3]
-const totalCountRollback = document.getElementsByClassName('total-input')[4]
+  toDoData.forEach(function (item) {
+    const li = document.createElement('li');
 
-let screens = document.querySelectorAll('.screen')
+    li.classList.add('todo-item');
 
-const appData = {
-  title: '',
-  screens: [],
-  countScreen: 0,
-  screenPrice: 0,
-  adaptive: true,
-  rollback: 0,
-  servicePricesPercent: 0,
-  servicePricesNumber: 0,
-  fullPrice: 0,
-  servicesPercent: {},
-  servicesNumber: {},
+    li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+    '<div class="todo-buttons">' +
+					'<button class="todo-remove"></button>' +
+					'<button class="todo-complete"></button>' +
+		'</div>'
 
-  init: function () {
-    appData.addTitle()
-    // appData.watchBtn()
-    startBtn.addEventListener('click', appData.start)
-    buttonPlus.addEventListener('click', appData.addScreenBlock)
-
-    inputRange.addEventListener('input', function(event) {
-      inputRangeValue.textContent = event.target.value + " %";
-      appData.rollback = event.target.value;
+    if (item.completed) {
+      toDoCompleted.append(li);
+    } else {
+      toDoList.append(li);
+    }
+    
+    li.querySelector('.todo-complete').addEventListener('click', function () {
+      item.completed = !item.completed
+      render();
     })
 
-  },
+    li.querySelector('.todo-remove').addEventListener('click', function () {
+      li.remove();
+      toDoData.splice( item, 1);
+      render();
 
-  addTitle: function () {
-    document.title = title.textContent
-  },
+      localStorage.setItem('toDoList', JSON.stringify(toDoData));
 
-  start: function () {
-    appData.addScreens()
-    appData.addServices()
+      if (toDoData.length === 0) {
+        localStorage.clear();
+      }
 
-    appData.addPrice()
+    });
 
-    //appData.logger()
-    // console.log(appData);
-    appData.showResult()
-  },
-
-  showResult: function () {
-    total.value = appData.screenPrice
-    totalCount.value = appData.countScreen
-    totalCountOther.value = appData.servicePricesPercent + appData.servicePricesNumber
-    fullTotalCount.value = appData.fullPrice
-    totalCountRollback.value = appData.servicePercentPrice
-  },
-
-  isNumber: function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num)
-  },
-
-  addScreens: function () {
-    screens = document.querySelectorAll('.screen')
-    screens.forEach(function(screen, index) {
-      const select = screen.querySelector('select')
-      const input = screen.querySelector('input')
-      const selectName = select.options[select.selectedIndex].textContent
-      appData.screens.push({
-        id: index, 
-        name: selectName, 
-        price: +select.value*+input.value,
-        count: +input.value
-      })
-    })
-    console.log(appData.screens)
-  },
-
-  addServices: function () {
-    otherItemsPercent.forEach(function(item) {
-        const check = item.querySelector('input[type=checkbox]')
-        const label = item.querySelector('label')
-        const input = item.querySelector('input[type=text]')
-  
-        if (check.checked) {
-          appData.servicesPercent[label.textContent] = +input.value
-        }
-      })
-      otherItemsNumber.forEach(function(item) {
-        const check = item.querySelector('input[type=checkbox]')
-        const label = item.querySelector('label')
-        const input = item.querySelector('input[type=text]')
-  
-        if (check.checked) {
-          appData.servicesNumber[label.textContent] = +input.value
-        }
-      })
-  },
-
-  addScreenBlock: function () {
-    const cloneScreen = screens[0].cloneNode(true)
-
-    screens[screens.length-1].after(cloneScreen)
-  },
-
-  addPrice: function () {
-    for (let screen of appData.screens) {
-      appData.screenPrice += +screen.price
+    if (toDoData.length >= 1) {
+      localStorage.setItem('toDoList', JSON.stringify(toDoData));
     }
 
-    for (let screen of appData.screens) {
-      appData.countScreen += +screen.count;
-    }
-
-    for(let key in appData.servicesNumber) {
-      appData.servicePricesNumber += appData.servicesNumber[key]
-    }
-
-    for(let key in appData.servicesPercent) {
-      appData.servicePricesPercent += appData.screenPrice * (appData.servicesPercent[key] / 100)
-    }
-
-    appData.fullPrice =  +appData.screenPrice + appData.servicePricesNumber + appData.servicePricesPercent
-
-    appData.servicePercentPrice =  Math.ceil(appData.fullPrice - (appData.fullPrice * (appData.rollback / 100)))
-  },
-//   logger: function () {
-//     console.log("allServicePrices", appData.allServicePrices);
-
-//     console.log(appData.getRollbackMessage(appData.fullPrice));
-//     console.log("title",typeof appData.title);
-//     console.log("screenPrice",typeof appData.screenPrice);
-//     console.log("adaptive",typeof appData.adaptive);
-
-//     console.log("screens.length",appData.screens.length);
-//     console.log("screens",appData.screens);
-//     console.log("serviePercentPrice",appData.serviePercentPrice);
-
-//     console.log("Стоимость верстки экранов " + appData.screenPrice + " гривен и Стоимость разработки сайта " + appData.fullPrice + " гривен.");
-//     console.log("Свойства и методы appData");
-//     for (let key in appData) {
-//       console.log( "Ключ: " + key + " Значение: " + appData[key] );
-//     }
-//   }
+  });
 }
 
-appData.init()
+const dataCheck = () => {
+  toDoData = localStorage.getItem('toDoList')
+  ? JSON.parse(localStorage.getItem('toDoList'))
+  : []
+  render();
+}
+
+todoControl.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  headerInput.value = headerInput.value.trim();
+
+  if (headerInput.value.length !== 0) {
+    
+  const newToDo = {
+    text: headerInput.value ,
+    completed: false
+  }
+
+  toDoData.push(newToDo)
+  headerInput.value = ''
+  render();
+ }
+})
+
+dataCheck();
