e35a3026dbc4bf3e33494813334ef1f9e57ffe55
Igor Golubev <igruliman@gmail.com>
2022-01-17 11:44:27 +0300

    Задание 13 todo комент

--- js/script.js
+++ js/script.js
@@ -7,7 +7,11 @@ const headerInput = document.querySelector('.header-input');
 const toDoList = document.querySelector('.todo-list');
 const toDoCompleted = document.querySelector('.todo-completed');
 
+// База данных
+
 let toDoData = [];
+
+// Объявление функции render
 
 const render = function () {
   toDoList.innerHTML = ''
@@ -24,16 +28,22 @@ const render = function () {
 					'<button class="todo-complete"></button>' +
 		'</div>'
 
+    // Проверка на выполненые заметки
+
     if (item.completed) {
       toDoCompleted.append(li);
     } else {
       toDoList.append(li);
     }
     
+    // Переключатель на выполнение заметки
+
     li.querySelector('.todo-complete').addEventListener('click', function () {
       item.completed = !item.completed
       render();
     })
+
+    // Удаление заметок
 
     li.querySelector('.todo-remove').addEventListener('click', function () {
       li.remove();
@@ -61,6 +71,8 @@ const dataCheck = () => {
   : []
   render();
 }
+
+// Отправка заметок в базу данных
 
 todoControl.addEventListener('submit', function(event) {
   event.preventDefault();
