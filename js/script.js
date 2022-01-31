29152ff764f8184cfb2214316a6e414f89ad4f15
Igor Golubev <igruliman@gmail.com>
2022-01-31 04:09:35 +0300

    Задание 15.ex

--- js/script.js
+++ js/script.js
@@ -1,42 +1,88 @@
 'use strict'
 
-// Объявление переменных в функции DomEl
+// Обработчик события 
 
-function DomElement (selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-}
+document.addEventListener('DOMContentLoaded', function (event) {
+  const px = 10;
 
-DomElement.prototype.newElement = function () {
-  let element;
+// Присвоение данных елементам
 
-  // Условие функции для селектора 
+  const DomElement = function (selector = '.block', styleAtr = {}) {
+      this.selector = selector === 'string' ? selector : (() => {
+          styleAtr = typeof selector === 'object' ? selector : {};
+          return '.block';
+      })();
+      this.height = '100px';
+      this.width = '100px';
+      this.bg = 'green';
+      this.fontSize = '10px';
+      this.cssText = '';
 
-  if (this.selector[0] === '.') {
-    element = document.createElement('div');
-    element.className = this.selector.slice(1);
-  }
-  if (this.selector[0] === '#') {
-    element = document.createElement('p');
-    element.id = this.selector.slice(1);
-  }
+// Объевление условий      
 
-  // Подключение стилей елементу
+      if (!('height' in styleAtr)) {
+          styleAtr.height = '100px';
+      }
+      if (!('width' in styleAtr)) {
+          styleAtr.width = '100px';
+      }
+      if (!('background' in styleAtr)) {
+          styleAtr.background = 'green';
+      }
+      if (!('font-size' in styleAtr)) {
+          styleAtr['font-size'] = '10px';
+      }
 
-  element.style.cssText = `height: ${this.height}px;
-      width: ${this.width}px;
-      background: ${this.bg};
-      font-size: ${this.fontSize}px;`;
-  return element;
-};
+      for (let key in styleAtr) {
+          this.cssText += `${key}: ${styleAtr[key]}; `;
+      }
+  };
+  
+  DomElement.prototype.newElement = function () {
+      let element;
+      
+// Объевление условий функции для селектора
 
-// Создание двух елементов на сайте
+      if (this.selector[0] === '.') {
+        element = document.createElement('div');
+        element.className = this.selector.slice(1);
+      }
+      
+      if (this.selector[0] === '#') {
+        element = document.createElement('p');
+        element.id = this.selector.slice(1);
+      }
+      
+      element.style.cssText = this.cssText;
+      element.style.cssText += `left: ${Math.round(window.innerWidth / 2 - parseInt(this.width) / 2)}px; `;
+      element.style.cssText += `top: ${Math.round(window.innerHeight / 2 - parseInt(this.height) / 2)}px; `;
+      
+      element.addEventListener('click', this.transfer);
+      
+      return element;
+  };
+  
+  let square = new DomElement({
+      position: 'absolute'
+  });
 
-let elementDiv = new DomElement('.block', 100, 200, 'green', 12);
-let elementParagraph = new DomElement('#best', 150, 400, 'red', 12);
+  document.body.appendChild(square.newElement());
+  
+  // Задание действий при нажатии стрелки на клавиатуре 
 
-document.body.appendChild(elementDiv.newElement());
-document.body.appendChild(elementParagraph.newElement());
+  document.addEventListener('keyup', (event) => {
+      const div = document.querySelector('div');
+      if (event.key === 'ArrowUp') {
+          div.style.top = parseInt(div.style.top) - px + 'px';
+      }
+      if (event.key === 'ArrowRight') {
+          div.style.left = parseInt(div.style.left) + px + 'px';
+      }
+      if (event.key === 'ArrowLeft') {
+          div.style.left = parseInt(div.style.left) - px + 'px';
+      }
+      if (event.key === 'ArrowDown') {
+          div.style.top = parseInt(div.style.top) + px + 'px';
+      }
+  });
+});
