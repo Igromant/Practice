d3cab917ef8f00be22cb88f5e0ccfdd13d048ad7
Igor Golubev <igruliman@gmail.com>
2022-02-09 10:10:13 +0300

    lessons 19-20 fix it

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,16 +1,22 @@
 const scroll = () => {
     // Объявление переменной и функция анимации
-    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    const smoothLinks = document.querySelectorAll('.link');
     for (let smoothLink of smoothLinks) {
         smoothLink.addEventListener('click', function (e) {
             e.preventDefault();
             const id = smoothLink.getAttribute('href');
     
-            document.querySelector(id).scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
         });
+
+        return id
+            
+        });
+
+        
     };
 }
 export default scroll
