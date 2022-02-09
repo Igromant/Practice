986969d6240d87a79aa59a328bd07d9b09372f2b
Igor Golubev <igruliman@gmail.com>
2022-02-09 08:53:41 +0300

    lessons 19-20 fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 import scroll from './modules/scroll'
-import valid from './modules/valid'
 
 timer('28 Feb 2022')
 menu()
 modal()
 scroll()
-valid()
