577a4630590e9f852ca85133da11ee78ab32b44d
Igor Golubev <igruliman@gmail.com>
2022-02-09 05:14:52 +0300

    lessons 19-20 and extra

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import timer from './modules/timer'
-
+import menu from './modules/menu'
+import modal from './modules/modal'
+import scroll from './modules/scroll'
 
 timer('28 Feb 2022')
+menu()
+modal()
+scroll()
