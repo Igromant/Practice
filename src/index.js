e33f84436d381564b58b728159f575106b5f163f
Igor Golubev <igruliman@gmail.com>
2022-02-09 06:01:14 +0300

    lessons 22

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
-import scroll from './modules/scroll'
 import valid from './modules/valid'
+import tabs from './modules/tabs'
+import scroll from './modules/scroll'
+
 
 timer('28 Feb 2022')
 menu()
 modal()
-scroll()
 valid()
+tabs()
+scroll()
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,44 +1,47 @@
 const modal = () => {
-    console.dir(document);
-    // Объявление переменных
-    const modal = document.querySelector(".popup");
-    const buttons = document.querySelectorAll(".popup-btn");
-    const closeBtn = modal.querySelector(".popup-close");
-  
-    // Функция открытия модального окна
-    const openModal = () => {
-      modal.style.display = "block";
-      document.querySelector(".popup-content").animate(
-        [
-          {
-            opacity: 0,
-            transform: "translate3D(0, -300px, 0)",
-          },
-          {
-            opacity: 1,
-          },
-        ],
+  // Объявление переменных
+  const modal = document.querySelector(".popup");
+  const buttons = document.querySelectorAll(".popup-btn")
+
+  // Функция открытия модального окна с анимацией
+  const openModal = () => {
+    modal.style.display = "block";
+    document.querySelector(".popup-content").animate(
+      [
         {
-          duration: 500,
-        }
-      );
-    };
-  
-    // Обработчик события нажатия кнопки
-    buttons.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        if (window.outerWidth > 768) {
-          openModal();
-        } else {
-          modal.style.display = "block";
-        }
-      });
-    });
-  
-    // Функция закрытия модального окна
-    closeBtn.addEventListener("click", () => {
-      modal.style.display = "none";
-    });
+          opacity: 0,
+          transform: "translate3D(0, -300px, 0)",
+        },
+        {
+          opacity: 1,
+        },
+      ],
+      {
+        duration: 500,
+      }
+    );
   };
-  
-  export default modal;
+
+  // События для кнопок
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      if (window.outerWidth > 768) {
+        openModal();
+      } else {
+        modal.style.display = "block";
+      }
+    });
+  });
+
+  // Событие модального окна
+  modal.addEventListener("click", (e) => {
+    if (
+      !e.target.closest(".popup-content") ||
+      e.target.classList.contains("popup-close")
+    ) {
+      modal.style.display = "none";
+    }
+  });
+};
+
+export default modal;
